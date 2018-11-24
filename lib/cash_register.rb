deedb0dc7ee14c4ad023a5c3b92bc5c1bd9dcbea
Will Figueiredo <will@wfigueiredo.com>
2018-11-24 20:29:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_transaction_amount
 
+  def initialize
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  
+end
